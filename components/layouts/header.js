f983a2aed529bca16518ea86be9d261a2a1c9b72
kimkundad <kim.kundad@gmail.com>
2023-02-24 01:14:41 +0700

    kim update

--- components/layouts/header.js
+++ components/layouts/header.js
@@ -1,14 +1,17 @@
-import { Box, Center, Image  } from '@chakra-ui/react'
+import { Box, Center, Image } from '@chakra-ui/react'
 import logo from '@/public/img/logo-shopee.png'
+import Link from 'next/link'
 
 export default function Header() {
 
-    
+
   return (
     <>
       <Box pb="48px">
         <Center w='100%' p="10px" bg="#f6f8fa" border='1px' borderColor='gray.200' pos="fixed">
-                <Image src={logo.src} alt='Logo website' h="45"/>
+          <Link href={'/'}>
+            <Image src={logo.src} alt='Logo website' h="45" />
+          </Link>
         </Center>
       </Box>
     </>
